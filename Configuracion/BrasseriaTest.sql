fc9fd32c6a3986c15af07ef7d9725ced926f8593
Joshua Saucedo <joshuansu0897@gmail.com>
2019-05-11 14:10:31 -0700

    Test Configurations

---
+++ Configuracion/BrasseriaTest.sql
@@ -0,0 +1,63 @@
+-- Adminer 4.7.1 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+
+DROP DATABASE IF EXISTS `BrasseriaTest`;
+CREATE DATABASE `BrasseriaTest` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `BrasseriaTest`;
+
+DROP TABLE IF EXISTS `Proveedor`;
+CREATE TABLE `Proveedor` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` text,
+  `createAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateAt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Product`;
+CREATE TABLE `Product` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `perecederos` tinyint(1) DEFAULT NULL,
+  `proveedor_id` int(11) NOT NULL,
+  `description` text,
+  `createAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateAt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `proveedor_id` (`proveedor_id`),
+  CONSTRAINT `Product_ibfk_2` FOREIGN KEY (`proveedor_id`) REFERENCES `Proveedor` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `Movements`;
+CREATE TABLE `Movements` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_id` int(11) NOT NULL,
+  `added_amount` int(11) NOT NULL,
+  `retired_amount` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `createAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateAt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `Movements_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `User`;
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` text NOT NULL,
+  `createAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `updateAt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2019-03-04 19:35:07
