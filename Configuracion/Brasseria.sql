e92578e1bccdc8bd8c2a615c8a285462b53b77e3
Joshua Saucedo <joshuansu0897@gmail.com>
2019-05-11 14:07:07 -0700

    the creation order of the tables was changed

--- Configuracion/Brasseria.sql
+++ Configuracion/Brasseria.sql
@@ -7,18 +7,14 @@ DROP DATABASE IF EXISTS `Brasseria`;
 CREATE DATABASE `Brasseria` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `Brasseria`;
 
-DROP TABLE IF EXISTS `Movements`;
-CREATE TABLE `Movements` (
+DROP TABLE IF EXISTS `Proveedor`;
+CREATE TABLE `Proveedor` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `product_id` int(11) NOT NULL,
-  `added_amount` int(11) NOT NULL,
-  `retired_amount` int(11) NOT NULL,
-  `total` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` text,
   `createAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `updateAt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `product_id` (`product_id`),
-  CONSTRAINT `Movements_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
@@ -37,14 +33,18 @@ CREATE TABLE `Product` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
-DROP TABLE IF EXISTS `Proveedor`;
-CREATE TABLE `Proveedor` (
+DROP TABLE IF EXISTS `Movements`;
+CREATE TABLE `Movements` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `description` text,
+  `product_id` int(11) NOT NULL,
+  `added_amount` int(11) NOT NULL,
+  `retired_amount` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
   `createAt` datetime DEFAULT CURRENT_TIMESTAMP,
   `updateAt` datetime DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `Movements_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Product` (`id`) ON DELETE CASCADE ON UPDATE RESTRICT
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
