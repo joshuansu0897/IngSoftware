00765290d991070bf47d4eb818aa3c36b5a7c5e4
Joshua Saucedo <joshuansu0897@gmail.com>
2019-05-12 08:07:32 -0700

    SpecificQuerys test

--- src/test/java/brasseria/db/SpecificQuerysNGTest.java
+++ src/test/java/brasseria/db/SpecificQuerysNGTest.java
@@ -5,6 +5,9 @@
  */
 package brasseria.db;
 
+import java.io.File;
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.ArrayList;
 import static org.testng.Assert.*;
 import org.testng.annotations.AfterClass;
@@ -18,16 +21,46 @@ import org.testng.annotations.Test;
  * @author joshuansu
  */
 public class SpecificQuerysNGTest {
-    
+
     public SpecificQuerysNGTest() {
     }
 
     @BeforeClass
     public static void setUpClass() throws Exception {
+        new File("Configuracion/config.json").renameTo(new File("Configuracion/configProduction.json"));
+        new File("Configuracion/configTest.json").renameTo(new File("Configuracion/config.json"));
+        try (Connection con = ProductoDB.getInstance().getConnection(); Statement stm = con.createStatement()) {
+            stm.execute("SET GLOBAL FOREIGN_KEY_CHECKS=0;");
+            stm.execute("INSERT INTO Product("
+                    + "	id, name, description, perecederos, proveedor_id) "
+                    + "	VALUES ('1', 'name', 'description', '1', '1')");
+            stm.execute("INSERT INTO Product("
+                    + "	id, name, description, perecederos, proveedor_id) "
+                    + "	VALUES ('2', 'name2', 'description2', '0', '2')");
+        }
+        try (Connection con = MovimientoDB.getInstance().getConnection(); Statement stm = con.createStatement()) {
+            stm.execute("SET GLOBAL FOREIGN_KEY_CHECKS=0;");
+            stm.execute("INSERT INTO Movements("
+                    + " product_id, added_amount, retired_amount, total) "
+                    + " VALUES ('1', '100', '40', '60')");
+            stm.execute("INSERT INTO Movements("
+                    + " product_id, added_amount, retired_amount, total) "
+                    + " VALUES ('2', '100', '0', '100')");
+        }
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
+        try (Connection con = MovimientoDB.getInstance().getConnection(); Statement stm = con.createStatement()) {
+            stm.execute("SET GLOBAL FOREIGN_KEY_CHECKS=1");
+            stm.execute("DELETE FROM Movements");
+        }
+        try (Connection con = ProductoDB.getInstance().getConnection(); Statement stm = con.createStatement()) {
+            stm.execute("SET GLOBAL FOREIGN_KEY_CHECKS=1");
+            stm.execute("DELETE FROM Product");
+        }
+        new File("Configuracion/config.json").renameTo(new File("Configuracion/configTest.json"));
+        new File("Configuracion/configProduction.json").renameTo(new File("Configuracion/config.json"));
     }
 
     @BeforeMethod
@@ -41,43 +74,45 @@ public class SpecificQuerysNGTest {
     /**
      * Test of getInstance method, of class SpecificQuerys.
      */
-    @Test(enabled=false)
+    @Test
     public void testGetInstance() {
         System.out.println("getInstance");
-        SpecificQuerys expResult = null;
-        SpecificQuerys result = SpecificQuerys.getInstance();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        SpecificQuerys instance = SpecificQuerys.getInstance();
+        assertNotNull(SpecificQuerys.getInstance());
+        assertEquals(instance, SpecificQuerys.getInstance());
     }
 
     /**
      * Test of getProductsAndCount method, of class SpecificQuerys.
      */
-    @Test(enabled=false)
+    @Test
     public void testGetProductsAndCount() throws Exception {
         System.out.println("getProductsAndCount");
-        SpecificQuerys instance = null;
-        ArrayList expResult = null;
-        ArrayList result = instance.getProductsAndCount();
-        assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        ArrayList<Object[]> expResult = new ArrayList();
+
+        Object[] obj = new Object[]{"name", 60};
+        expResult.add(obj);
+
+        Object[] obj2 = new Object[]{"name2", 100};
+        expResult.add(obj2);
+
+        ArrayList result = SpecificQuerys.getInstance().getProductsAndCount();
+
+        result.forEach(p -> {
+            assertNotNull(expResult.stream().allMatch(ex -> ex.equals(p)));
+        });
     }
 
     /**
      * Test of getProductsAndCountByName method, of class SpecificQuerys.
      */
-    @Test(enabled=false)
+    @Test
     public void testGetProductsAndCountByName() throws Exception {
         System.out.println("getProductsAndCountByName");
-        String name = "";
-        SpecificQuerys instance = null;
-        Object[] expResult = null;
-        Object[] result = instance.getProductsAndCountByName(name);
+        String name = "name";
+        Object[] expResult = new Object[]{"name", 0, 0, "60"};
+        Object[] result = SpecificQuerys.getInstance().getProductsAndCountByName(name);
         assertEquals(result, expResult);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-    
+
 }
